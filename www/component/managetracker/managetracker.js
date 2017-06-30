2333f3f0c23a41d7114f72dbde2cd70193f91f7b
user-PC\user <geeta@velankanigroup.com>
2017-06-30 19:04:06 +0530

    no message

--- www/component/managetracker/managetracker.js
+++ www/component/managetracker/managetracker.js
@@ -1,17 +1,7 @@
 angular.module('managetracker', [])
     .controller('ManageTrackerCtrl', function ($scope, $state, $ionicModal, $timeout, PageConfig, BatsServices, ionicToast, $interval,
         Constants, UtilsFactory) {
-        // if (UtilsFactory.getNotificationDetails()) {
-        //     console.log(UtilsFactory.getNotificationDetails());
-        //     $scope.notificationData = UtilsFactory.getNotificationDetails();
-        //     $scope.count = UtilsFactory.getNotificationCount();
-        //     console.log($scope.count);
-        //     if ($scope.count == undefined) {
-        //         $scope.count = 0;
-        //         $scope.notificationData = [];
-        //     }
-        // }
-
+     
         function init() {
             var inputParam = {};
             BatsServices.deviceList(inputParam).success(function (response) {
