8d2d60c3878fed6af8b6f89ac530e99f49c37da6
BISDT316\Geeta <geetasinghrajput99@gmail.com>
2017-07-10 16:54:34 +0530

    activate multiple device and calender invalide time red color

--- www/component/managetracker/managetracker.js
+++ www/component/managetracker/managetracker.js
@@ -1,29 +1,40 @@
 angular.module('managetracker', [])
     .controller('ManageTrackerCtrl', function ($scope, $state, $ionicModal, $timeout, PageConfig, BatsServices, ionicToast, $interval,
         Constants, UtilsFactory) {
-     
+        var intervaltime = 15;
         function init() {
             var inputParam = {};
             BatsServices.deviceList(inputParam).success(function (response) {
-                $scope.trackerList = response
+                $scope.trackerList = response;
+               // checkDeviceIsPending();
             }).error(function (error) {
-                if(error.err=='Origin Server returned 504 Status'){
-                     ionicToast.show('Internet is very slow', Constants.TOST_POSITION, false, Constants.TIME_INTERVAL);
+                if (error.err == 'Origin Server returned 504 Status') {
+                    ionicToast.show('Internet is very slow', Constants.TOST_POSITION, false, Constants.TIME_INTERVAL);
                 }
-                else{
+                else {
                     ionicToast.show(error.err, Constants.TOST_POSITION, false, Constants.TIME_INTERVAL);
                 }// ionicToast.show(error, Constants.TOST_POSITION, false, Constants.TIME_INTERVAL);
             });
         }
         init();
 
+        function checkDeviceIsPending() {
+           // $scope.trackerList
+
+            var status = _.findIndex($scope.trackerList, { status: 'Pending' });
+            if (status != -1) {
+                callDeviceLIstAPI();//  init();
+                $scope.countingForActivation++;
+            }
+        }
         $scope.countingForActivation = 0;
         $scope.$on('manageTrackerList', function () {
             console.log("im redirection from activation" + $scope.countingForActivation);
             // $scope.countingForActivation=1;
             init();
             if ($scope.countingForActivation > 0) {
-
+                intervaltime = intervaltime + $scope.time + 1;
+                console.log("interval tie changed to " + intervaltime);
             } else {
                 callDeviceLIstAPI();//  init();
             }
@@ -49,7 +60,7 @@ angular.module('managetracker', [])
                 BatsServices.deviceList(inputParam).success(function (response) {
                     $scope.trackerList = response
                     console.log("inside " + $scope.time + " interval lenght " + $interval.length);
-                    if ($scope.time === 15) {
+                    if ($scope.time === intervaltime) {
                         console.log("inside manage tracker interval end ");
                         init();
                         $interval.cancel(deviceListAPi);
@@ -64,7 +75,7 @@ angular.module('managetracker', [])
                     }// ionicToast.show(error, Constants.TOST_POSITION, false, Constants.TIME_INTERVAL);
                 });
             } else {
-                console.log("inside manage tracker interval end ");
+                console.log(" change page inside manage tracker interval end ");
                 $interval.cancel(deviceListAPi);
             }
         }
